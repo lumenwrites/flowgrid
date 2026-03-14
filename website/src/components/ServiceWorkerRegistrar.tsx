5f6407e73cbc31e018d61b009d89c56a806ce75e
lumenwrites <lumenwrites@gmail.com>
2026-03-14 14:26:46 +0100

    Fix cache in dev

--- website/src/components/ServiceWorkerRegistrar.tsx
+++ website/src/components/ServiceWorkerRegistrar.tsx
@@ -4,9 +4,18 @@ import { useEffect } from 'react'
 
 export function ServiceWorkerRegistrar() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js')
+    if (!('serviceWorker' in navigator)) return
+
+    if (process.env.NODE_ENV === 'development') {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (const registration of registrations) {
+          registration.unregister()
+        }
+      })
+      return
     }
+
+    navigator.serviceWorker.register('/sw.js')
   }, [])
 
   return null
